f9714f61564d5de7863d3995ef3e1ec4b1f1093d
Raphael Rugna <raphaelrugna@gmail.com>
2020-07-27 12:37:28 -0300

    Criação de formulário de cadastro e função js para preenchimento da tabela html

--- js/principal.js
+++ js/principal.js
@@ -36,5 +36,54 @@ for(var i = 0; i < pacientes.length; i++){
     }
 }
 
+// FUNÇÕES JS:
 
+// titulo.addEventListener("click", function(){                     // Função Anônima
+//     console.log("Olha só, posso chamar uma função anônima!");
+// });
 
+// titulo.addEventListener("click", mostraMensagem);                // Função Nomeada
+// function mostraMensagem(){                                       
+//     console.log("Olá eu fui clicado!");
+// }
+
+var botaoAdicionar = document.querySelector("#adicionar-paciente");
+botaoAdicionar.addEventListener("click", function(event){
+    event.preventDefault(); // -------------------------------------->>> Previne comportamento padrão
+    var form = document.querySelector("#form-adiciona");
+
+    var nome = form.nome.value;
+    var peso = form.peso.value;
+    var altura = form.altura.value;
+    var gordura = form.gordura.value;
+
+    var pacienteTr = document.createElement("tr");
+    var nomeTd = document.createElement("td");
+    nomeTd.classList.add("info-nome");
+    var pesoTd = document.createElement("td");
+    pesoTd.classList.add("info-peso");
+    var alturaTd = document.createElement("td");
+    alturaTd.classList.add("info-altura");
+    var gorduraTd = document.createElement("td");
+    gorduraTd.classList.add("info-gordura");
+    var imcTd = document.createElement("td");
+    imcTd.classList.add("info-imc");
+
+    nomeTd.textContent = nome;
+    pesoTd.textContent = peso;
+    alturaTd.textContent = altura;
+    gorduraTd.textContent = gordura;
+    imcTd.textContent = 0;
+
+    pacienteTr.appendChild(nomeTd);
+    pacienteTr.appendChild(pesoTd);
+    pacienteTr.appendChild(alturaTd);
+    pacienteTr.appendChild(gorduraTd);
+    pacienteTr.appendChild(imcTd);
+
+    var tabela = document.querySelector("#tabela-pacientes");
+    tabela.appendChild(pacienteTr);
+
+    console.log(nomeTd);
+
+});
