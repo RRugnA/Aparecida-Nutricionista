69fc97c91873f082cb251543f14985fc5202267c
Raphael Rugna <raphaelrugna@gmail.com>
2020-07-29 23:10:11 -0300

    Filtro de busca por nome de paciente

---
+++ js/filtra.js
@@ -0,0 +1,25 @@
+var campoFiltro = document.querySelector("#filtrar-tabela");
+
+campoFiltro.addEventListener("input", function(){
+    console.log(this.value);
+    var pacientes = document.querySelectorAll(".paciente");
+
+    if(this.value.length > 0){
+        pacientes.forEach(function(paciente){
+            var tdNome = paciente.querySelector(".info-nome");
+            var nome = tdNome.textContent;
+            var expressao = new RegExp(campoFiltro.value, "i");
+            if(!expressao.test(nome)){
+                paciente.classList.add("invisivel");
+            }else{
+                paciente.classList.remove("invisivel");
+            }
+        });
+    }else{
+        for (let i = 0; i < pacientes.length; i++) {
+            var paciente = pacientes[i];
+            paciente.classList.remove("invisivel");
+            
+        }
+    }
+});
